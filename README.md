9d6415ad14fd570ade61cacb0ccefdef2cff7f28
taavirannamets <taavirannamets@gmail.com>
2022-11-23 19:59:55 +0200

    final touches and readme file

---
+++ README.md
@@ -0,0 +1,5 @@
+# Simple Face Detection
+
+![Screenshot](/other/scr1.png)
+![Screenshot](/other/scr2.png)
+![Screenshot](/other/scr3.png)
--- script.js
+++ script.js
@@ -6,18 +6,27 @@ Promise.all([
 	faceapi.nets.ssdMobilenetv1.loadFromUri('/models'),
 ]).then(start)
 
-function start() {
+async function start() {
 	const container = document.createElement('div')
 	container.style.position = 'relative'
 	document.body.append(container)
 
+	const labeledFaceDescriptors = await loadLabeledImages()
+	const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6)
+
+	let image
+	let canvas
+
 	document.body.append('Loaded')
 
 	imageUpload.addEventListener('change', async () => {
-		const image = await faceapi.bufferToImage(imageUpload.files[0])
+		if (image) image.remove()
+		if (canvas) canvas.remove()
+
+		image = await faceapi.bufferToImage(imageUpload.files[0])
 		container.append(image)
 
-		const canvas = faceapi.createCanvasFromMedia(image)
+		canvas = faceapi.createCanvasFromMedia(image)
 		container.append(canvas)
 		const displaySize = { width: image.width, height: image.height }
 		faceapi.matchDimensions(canvas, displaySize)
@@ -28,10 +37,44 @@ function start() {
 			.withFaceDescriptors()
 
 		const resizedDetections = faceapi.resizeResults(detections, displaySize)
-		resizedDetections.forEach(detection => {
-			const box = detection.detection.box
-			const drawBox = new faceapi.draw.DrawBox(box, { label: 'Face' })
+		const results = resizedDetections.map(d =>
+			faceMatcher.findBestMatch(d.descriptor)
+		)
+		results.forEach((result, i) => {
+			const box = resizedDetections[i].detection.box
+			const drawBox = new faceapi.draw.DrawBox(box, {
+				label: result.toString(),
+			})
 			drawBox.draw(canvas)
 		})
 	})
+}
+
+function loadLabeledImages() {
+	const labels = [
+		'Black Widow',
+		'Captain America',
+		'Captain Marvel',
+		'Hawkeye',
+		'Jim Rhodes',
+		'Thor',
+		'Tony Stark',
+	]
+	return Promise.all(
+		labels.map(async label => {
+			const descriptions = []
+			for (let i = 1; i <= 2; i++) {
+				const img = await faceapi.fetchImage(
+					`./labeled_images/${label}/${i}.jpg`
+				)
+				const detections = await faceapi
+					.detectSingleFace(img)
+					.withFaceLandmarks()
+					.withFaceDescriptor()
+				descriptions.push(detections.descriptor)
+			}
+
+			return new faceapi.LabeledFaceDescriptors(label, descriptions)
+		})
+	)
 }
